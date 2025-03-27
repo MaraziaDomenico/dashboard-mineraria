5e837cd906729c486dca2984f3bc568dff0a554c
Domenico <marazia.domenico@gmail.com>
2025-03-27 15:12:47 +0100

    Aggiorna config.py per supportare DATABASE_URL su Railway

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
+import os
 from configparser import ConfigParser
+import urllib.parse as up
 
 def config(filename="database.ini", section="postgresql"):
+    # Se DATABASE_URL è presente (es. Railway), parse e restituisce i parametri
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url:
+        up.uses_netloc.append("postgres")
+        url = up.urlparse(database_url)
+
+        return {
+            "host": url.hostname,
+            "database": url.path[1:],  # rimuove lo slash iniziale
+            "user": url.username,
+            "password": url.password,
+            "port": url.port or 5432
+        }
+
+    # Altrimenti legge dal file .ini (es. in locale)
     parser = ConfigParser()
     parser.read(filename)
 
